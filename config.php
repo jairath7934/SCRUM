65deae8a32579cfc712c6111a83785a266708a9a
jairath7934 <jairath7934@conestogac.on.ca>
2025-02-12 19:46:14 -0500

    database

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost:3307';
+$dbname = 'GiftShopDB';
+$username = 'root';
+$password = 'password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
